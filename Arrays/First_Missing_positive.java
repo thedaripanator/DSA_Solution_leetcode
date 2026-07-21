6f38cce135291ff2c07d8e3629bf6c1fb060df17
thedaripanator <sayondeep1618@gmail.com>
2026-07-21 10:25:42 +0530

    Solved First Missing Positive problem of leetcode

---
+++ Arrays/First_Missing_positive.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class First_Missing_positive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(firstMissingPositive(arr));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int length=nums.length;
+        for(int i=0;i<length;i++){
+            while(nums[i]>0 && nums[i]<=length && nums[i]!=nums[nums[i]-1]){
+                int index=nums[i]-1;
+                int temp=nums[index];
+                nums[index]=nums[i];
+                nums[i]=temp;
+            }
+        }
+        for(int i=0;i<length;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return length+1;
+    }
+}
